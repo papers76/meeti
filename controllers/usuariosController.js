d8501f5512a1c80d6d5e2bf4e249fcb97d885e5b
Ariel Basabe <lazurus.kilix@gmail.com>
2024-04-12 00:18:01 -0300

    5. Comenzando el Código para mostrar alertas

--- controllers/usuariosController.js
+++ controllers/usuariosController.js
@@ -9,9 +9,22 @@ exports.formCrearCuenta =  (req, res) => {
 exports.crearNuevaCuenta = async (req, res) => {
         const usuario = req.body;
 
-        const nuevoUsuario = await Usuarios.create(usuario);  
+        try {
+            const nuevoUsuario = await Usuarios.create(usuario);  
 
         //TODO :flash Message y redireccionar
 
         console.log("Usuario creado", nuevoUsuario);
+            
+        
+    } catch (error) {
+            const erroresSequelize = error.errors.map(err => err.message);
+            
+            // console.log(erroresSequelize); 
+            req.flash('error', erroresSequelize);
+            res.redirect('/crear-cuenta');
+        }
+
+        
     }
+
